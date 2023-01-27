a9ba6c3bffc04f7b535deb8987c6a713eb664476
Derek Cortez <derekcortez16@gmail.com>
2023-01-26 20:34:04 -0600

    Geolocalizacion mejorada

--- app/src/main/java/dgtic/unam/proyectoandroid/MapsActivity.kt
+++ app/src/main/java/dgtic/unam/proyectoandroid/MapsActivity.kt
@@ -2,42 +2,32 @@ package dgtic.unam.proyectoandroid
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Dialog
-import android.content.DialogInterface
 import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.graphics.Color
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.WindowManager
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.DialogFragment
 import com.google.android.gms.common.api.ResolvableApiException
-import android.location.Location
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
 
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.gms.tasks.Task
 import dgtic.unam.proyectoandroid.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
 
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-
-    private val SOLICITAR_ACCES_FINE_LOCATION = 1000
-    private lateinit var locationRequest: LocationRequest
-    private lateinit var locationCallback: MyLocationCallback
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,54 +41,56 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        iniciarGeolocalizacion()
-
     }
 
-    private fun iniciarGeolocalizacion() {
-        fusedLocationProviderClient = getFusedLocationProviderClient(this)
-        locationCallback = MyLocationCallback()
-        locationRequest = LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 5000
-        locationRequest.fastestInterval = 5000
-    }
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
-
-    //verificamos los permisos de ubicacion del usuario
-    private fun verificarPermisos(cancel: ()-> Unit, ok: ()-> Unit){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-                //se nego los permisos
-                cancel()
-            }else{
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), SOLICITAR_ACCES_FINE_LOCATION)
-            }
+    private fun enableLocation(){
+        if(!::mMap.isInitialized) return
+        if(isLocationPermissionGranted()){
+            mMap.isMyLocationEnabled = true
         }else{
-            //encender gps del usuario
-            val request = LocationRequest.create().apply {
-                interval = 2000
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            }
-            val builder= LocationSettingsRequest.Builder().addLocationRequest(request)
-            val client: SettingsClient = LocationServices.getSettingsClient(this)
-            val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
-            task.addOnFailureListener{
-                if (it is ResolvableApiException){
-                    try{
-                        it.startResolutionForResult(this, 12345)
-                    }catch (sendEx: IntentSender.SendIntentException){
-                    }
-                }
-            }.addOnSuccessListener {
-                //el gps se enciende
-            }
-            //se activo el gps
-            ok()
+            requestLocationPermission()
         }
     }
-    private fun mostrarDialogoPermiso(){
-        ActivityCompat.requestPermissions(this@MapsActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), SOLICITAR_ACCES_FINE_LOCATION)
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this, "Acepta los permisos para usar la app", Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMap.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(this,
+                    "Para activar la localizacion ve a ajustes y acepta los permisos",
+                    Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+
+        }
+
+
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if(!::mMap.isInitialized) return
+        if(!isLocationPermissionGranted()){
+            mMap.isMyLocationEnabled = false
+            Toast.makeText(this,
+                "Para activar la localizacion ve a ajustes y acepta los permisos",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 
     /**
@@ -112,69 +104,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        createMarker()
+        mMap.setOnMyLocationButtonClickListener(this)
+        mMap.setOnMyLocationClickListener (this)
+        enableLocation()
+    }
+    private fun createMarker(){
+        val favoritePlace = LatLng(28.044195, -16.5363842)
+        mMap.addMarker(MarkerOptions().position(favoritePlace).title("Mi playa favorita!"))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(favoritePlace, 18f), 4000, null)
     }
 
-    override fun onResume() {
-        super.onResume()
-        verificarPermisos(cancel = {
-            //moestramos el dialogo para solicitarle al usuario que encienda su gps
-            mostrarDialogoPermiso()
-        }, ok = {
-            //iniciamos la geolocalizacion del usuario con un listener o oyente
-            agregarOyenteUbicacion()
-        })
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Ubicacion actual", Toast.LENGTH_SHORT).show()
+        return false
     }
 
-    override fun onPause() {
-        super.onPause()
-        removeLocationListener()
-    }
-
-    private fun removeLocationListener(){
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun agregarOyenteUbicacion(){
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
-    }
-
-    //Callback para mostrar un marcador en el mapa de google
-    inner class MyLocationCallback: LocationCallback(){
-        override fun onLocationResult(locationResult: LocationResult) {
-            super.onLocationResult(locationResult)
-
-            val location = locationResult.lastLocation
-
-            location?.run {
-                val latLng = LatLng(latitude, longitude)
-
-                mMap.addMarker(MarkerOptions().position(latLng).title("Mi ubicacion"))
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-                Log.e("Ubicacion", "Latitud: $latitude, Longitud: $longitude")
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            SOLICITAR_ACCES_FINE_LOCATION->{
-                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
-                    //ESTE ETODO HACE USO DE FUSED LOCATION PROVIDER
-                    agregarOyenteUbicacion()
-                }else{
-                    //si el usuario denego el permiso le mostramos el mensaje "permiso denegado"
-                    val toast = Toast.makeText(applicationContext, "Permiso denegado", Toast.LENGTH_SHORT)
-                    toast.show()
-                }
-                return
-            }
-        }
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Estas en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
     }
 
 
